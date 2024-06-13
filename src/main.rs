8f21ce6826dee33645888ab87b7ab27377f37818
Denis <denis.boghiu@gmail.com>
2024-06-13 21:25:38 +0300

    update style

--- src/main.rs
+++ src/main.rs
@@ -47,23 +47,27 @@ impl TypeState {
 }
 #[component]
 fn App() -> impl IntoView {
-    let (store, set_store) = create_signal(TypeState::from_str("Mit seinem Nein zu Koalitionen."));
+    let (store, set_store) = create_signal(TypeState::from_str("Dass überhaupt noch Euro und Dollar in Moskau landen und gegen Rubel getauscht werden können, ist dabei wenig überraschend. Russland erwirtschaftet Jahr für Jahr hohe Überschüsse durch den Verkauf von Öl, Gas und anderen Rohstoffen. Viele davon werden auch nach Kriegsbeginn noch in westlichen Währungen bezahlt, auch wenn der Anteil asiatischer Währungen wie des chinesischen Yuan steigt. Russlands Exporteure also haben Dollar und Euro, während andere Firmen im Ausland dringend benötigte Einfuhren kaufen müssen. Auch von diesen müssen viele immer noch in Euro und Dollar bezahlt werden. Grob vereinfacht gesagt: Russlands Rohstoffexporteure verkaufen in Moskau ihre Dollars, die Importeure wiederum kaufen sie dort. Einer der zentralen Handelspunkte für solche Geschäfte war bislang: die Moskauer Börse."));
     view! {
         <div
-            class="board auto flex"
+            class="w-screen p-10 flex flex-wrap text-2xl text-gray-500 focus:bg-gray-300 font-mono"
             tabindex=1
             on:keydown=move |event| {
                 let key = event.key_code();
                 let mut local_store = store.get();
-                logging::log!("current index {}", local_store.index);
+                logging::log!("key down {}", key);
                 if key == 8 && local_store.index > 0 {
                     local_store.index -= 1;
                     let temp = local_store.data.get_mut(local_store.index).unwrap();
                     temp.backspace();
                     set_store(local_store);
-                } else if key == 32 && local_store.index < local_store.data.len() {
+                } else if (key == 32) && local_store.index < local_store.data.len() {
                     let temp = local_store.data.get_mut(local_store.index).unwrap();
-                    temp.typed(char::from_u32(key).unwrap());
+                    let key = match key {
+                        190 => '.',
+                        x => char::from_u32(x).unwrap(),
+                    };
+                    temp.typed(key);
                     local_store.index += 1;
                     set_store(local_store);
                 }
@@ -72,19 +76,21 @@ fn App() -> impl IntoView {
             on:keypress=move |event| {
                 let key = event.key_code();
                 match key {
-                    (64..=93) | (97..=122) => {
+                    (64..=93) | (97..=122) | 44 | 45 | 46 | 58 | 59 => {
                         let mut local_store = store.get();
                         if local_store.index < local_store.data.len() {
                             logging::log!("current index {}", local_store.index);
                             let temp = local_store.data.get_mut(local_store.index).unwrap();
+                            logging::log!("inserting {}", char::from_u32(key).unwrap());
                             temp.typed(char::from_u32(key).unwrap());
+
                             local_store.index += 1;
                             set_store(local_store);
                         }
                     }
                     _ => {}
                 };
-                logging::log!("{:?}", & key);
+                logging::log!("keypress {:?}", & key);
             }
         >
 
@@ -93,20 +99,18 @@ fn App() -> impl IntoView {
                     <For
                         each=move || store.get().data.into_iter().enumerate()
                         key=|(index, c)| {
-                            format!("{}-{}", index.clone(), c.typed_char.unwrap_or('-'))
+                            format!("{}-{}", index.clone(), c.typed_char.unwrap_or('~'))
                         }
 
                         children=move |(_index, c)| {
                             if let Some(typed_char) = c.typed_char {
-                                if typed_char == c.reference_char {
-                                    return view! {
-                                        <div class="p-2 m-1 shadow-lg">{c.reference_char}</div>
-                                    };
+                                if compare( typed_char , c.reference_char) {
+                                    return view! { <div class="min-w-4 text-gray-900">{c.reference_char}</div> };
                                 } else {
                                     return view! {
-                                        <div class="p-2 m-1 shadow-lg relative text-gray-400">
+                                        <div class="relative text-gray-400 min-w-4">
                                             {c.reference_char}
-                                            <div class="absolute -top-2 -right-1 text-red-600">
+                                            <div class="absolute -top-0 -right-0 text-red-600 italic text-sm">
                                                 <p>{c.typed_char}</p>
                                             </div>
                                         </div>
@@ -114,7 +118,7 @@ fn App() -> impl IntoView {
                                 }
                             }
                             view! {
-                                <div class="p-2 m-1 shadow-lg text-amber-600">
+                                <div class="min-w-4">
                                     {c.reference_char} {c.typed_char}
                                 </div>
                             }
@@ -124,5 +128,29 @@ fn App() -> impl IntoView {
             }
 
         </div>
+    }
+}
+
+fn compare(t: char, r: char) -> bool {
+    if t == r {
+        true
+    } else if t == 'S' && r == 'ß' {
+        true
+    } else if t == 'U' && r == 'Ü' {
+        true
+    } else if t == 'A' && r == 'Ä' {
+        true
+    } else if t == 'O' && r == 'Ö' {
+        true
+    } else if t == 's' && r == 'ß' {
+        true
+    } else if t == 'u' && r == 'ü' {
+        true
+    } else if t == 'a' && r == 'ä' {
+        true
+    } else if t == 'o' && r == 'ö' {
+        true
+    } else {
+        false
     }
 }
