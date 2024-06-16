a3bb3407942f1c9493ac2c1c6360c5893169de6b
Denis <denis.boghiu@gmail.com>
2024-06-16 22:47:54 +0300

    instruction to run translation server on AWS

--- readme.md
+++ readme.md
@@ -14,6 +14,20 @@ leptosfmt .
 
 using https://huggingface.co/docs/transformers/model_doc/marian
 
+#### dependencies
+
+AWS EC2
+
+use 16GB of storage (after the installation uses 7.8GB of store, during the installation up to 12GB)
+
+use t2.medium to install and t2.small to run
+
+```python
+sudo yum install python pip
+mkdir tmp
+TMPDIR=/home/ec2-user/tmp pip install torch flask transformers sentencepiece sacremoses --no-cache-dir
+```
+
 ```python
 python translate_server.py
 ```
--- translate_server.py
+++ translate_server.py
@@ -10,9 +10,7 @@ app = Flask(__name__)
 @app.route('/translate', methods=['POST'])
 def translate():
     src_text = json.loads(request.data)["src"]
-    # src_text = request.args.get('text')
-    # src_text = ["was machen wir jetzt?"]
     translated = model.generate(**tokenizer(src_text, return_tensors="pt", padding=True))
     return jsonify({'translated': [tokenizer.decode(t, skip_special_tokens=True) for t in translated]})
 print ("start application")
-app.run(debug=False)
+app.run(debug=False,host="0.0.0.0")
