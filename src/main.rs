e81ce0427dc90d135aeeaa2259ba4dd40bff2dad
Denis <denis.boghiu@gmail.com>
2024-06-29 10:52:42 +0300

    input text

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
+    let (translation_input, set_translation_input) = create_signal("".to_string());
     let sentances = vec![
         "Mit intelligenten Stromzählern können Verbraucher selbst am Energiemarkt teilnehmen. Wie Sie Geld sparen und sogar welches verdienen.",
         "Die Preise an der Strombörse fahren an vielen Tagen des Jahres Achterbahn: Sie vervielfachen sich oft binnen weniger Stunden, um kurz darauf genauso rasant wieder abzustürzen. Mitunter gar in den negativen Bereich – die Versorger bekommen dann Geld dafür, dass sie Strom abnehmen.",
@@ -30,7 +31,7 @@ fn App() -> impl IntoView {
                 <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
                     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
                         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 lg:p-5">
-                            <div class="relative transform overflow-hidden bg-gray-100 shadow-xl transition-all w-full h-full">
+                            <div class="flex relative transform overflow-hidden bg-gray-100 shadow-xl transition-all w-full h-full">
                                 <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                                     <Popup text=t translation=tr display=None/>
                                     <input
@@ -43,9 +44,49 @@ fn App() -> impl IntoView {
                         </div>
                     </div>
                 </div>
-            }
+            }.into_view()
         } else {
-            view! { <div></div> }
+            view! {}.into_view()
+        }
+    };
+
+    let (input_popup, set_input_popup) = create_signal(false);
+
+    let input_popup_component = move || {
+        if input_popup() {
+            view! {
+                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+                    <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 lg:p-5">
+                            <div class="flex relative transform overflow-hidden bg-gray-100 shadow-xl transition-all w-full h-full">
+                                <div class="flex flex-1 flex-col bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                    <textarea
+                                        class="h-80"
+                                        type="textarea"
+                                        placeholder="type here your text"
+                                        prop:value=translation_input
+                                        on:input=move |event| {
+                                            set_translation_input.set(event_target_value(&event));
+                                        }
+                                    >
+                                    </textarea>
+                                    <input
+                                        type="button"
+                                        value="Close"
+                                        on:click=move |_event| {
+                                            logging::log!("{}", translation_input.get());
+                                            set_input_popup.set(false);
+                                        }
+                                    />
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }.into_view()
+        } else {
+            view! {}.into_view()
         }
     };
 
@@ -58,11 +99,13 @@ fn App() -> impl IntoView {
         .collect_view();
 
     view! {
+        <div class="p-3 pt-7 lg:text-3xl text-5xl font-bold text-gray-100 font-mono w-screen items-center flex flex-col snap-start">
+            <div on:click=move |_event| set_input_popup(true)>Update text!</div>
+        </div>
         <div class="w-screen flex flex-col items-center">
+            <div>{move || input_popup_component}</div>
             <div>{move || popup_component}</div>
-            <div class="w-screen lg:w-3/4 flex flex-col">
-                {views}
-            </div>
+            <div class="w-screen lg:w-3/4 flex flex-col">{views}</div>
         </div>
     }
 }
